a80914b16ef381968908965fe1db68ec9aef071f
Pavel Piasetski <pavel.piasetski@sugarcrm.com>
2023-11-17 17:32:30 +0300

    CC-11 Bugfixing (in progress)

--- processor.py
+++ processor.py
@@ -1,4 +1,6 @@
-from sqlalchemy import create_engine, select, insert, update
+from datetime import datetime
+
+from sqlalchemy import create_engine, select, insert, update, delete
 from sqlalchemy.orm import Session
 
 import settings
@@ -11,10 +13,15 @@ session = Session(engine)
 
 
 class DatabaseHandler:
+    DEFAULT_USER_ID = 1
+
+    TEST_DEFAULT_AFFECTED_ROW_COUNT = 2
+
     def __init__(self, current_session):
         self.session = current_session
 
     def handle(self):
+        self._delete_duplicated_news_or_extra_staff()
         news_contents = self._get_news_content_list()
         for news_content in news_contents:
             parse_service = ParseTextFromHtmlService(news_content.text)
@@ -27,27 +34,46 @@ class DatabaseHandler:
             parse_service = ParseTextFromHtmlService(news_content.text)
 
             news_content = self._extract_text_news_content_text(news_content, parse_service)
-            self._extract_image_urls_into_assets(news_content, parse_service)
-            self._extract_youtube_urls_into_news_content_view_data(news_content, parse_service)
+            if not news_content.assets:
+                self._extract_image_urls_into_assets(news_content, parse_service)
+                self._extract_youtube_urls_into_news_content_view_data(news_content, parse_service)
         self.session.commit()
 
+    def _delete_duplicated_news_or_extra_staff(self):
+        news_content_stmt = delete(models.NewsContent).where(models.NewsContent.text == '')
+        self.session.execute(news_content_stmt)
+
     def _clean_view_data_field(self):
-        news_content_stmt = update(models.NewsContent).values(view_data=None)
+        #TODO remove 'limit()' for production
+        news_content_stmt = update(models.NewsContent).values(view_data=None).where(
+            models.NewsContent.news_content_id.in_([322, 320]))
+
         self.session.execute(news_content_stmt)
 
     def _get_news_content_list(self):
-        news_content_stmt = select(models.NewsContent).order_by(models.NewsContent.news_content_id.desc()).limit(2)     #TODO remove 'limit()' for production
+        news_content_stmt = select(models.NewsContent).order_by(models.NewsContent.news_content_id.desc()).limit(
+            self.TEST_DEFAULT_AFFECTED_ROW_COUNT)  # TODO remove 'limit()' for production
         return self.session.scalars(news_content_stmt).all()
 
     def _extract_text_news_content_text(self, news_content, parse_service):
         news_content.text = parse_service.parse_text()
+        news_content.updated_date = datetime.utcnow()
         return news_content
 
     def _extract_image_urls_into_assets(self, news_content, parse_service):
         image_urls = parse_service.parse_image_urls()
+        file_name = image_urls.split('/')[-1]
+
         for image_path in image_urls:
-            file_name = self._upload_file_by_file_path(image_path)
-            self._create_assets_by_img_url(news_content.news_content_id, file_name)
+            asset = self._get_asset_by_file_name(file_name)
+            if asset:
+                news_content_asset_stmt = insert(models.NewsContentAssets).values(
+                    news_content_id=news_content.news_content_id,
+                    asset_id=asset.asset_id)
+                self.session.execute(news_content_asset_stmt)
+            else:
+                file_name = self._upload_file_by_file_path(image_path)
+                self._create_assets_by_img_url(news_content.news_content_id, file_name)
 
     @staticmethod
     def _upload_file_by_file_path(file_path):
@@ -56,11 +82,12 @@ class DatabaseHandler:
         return file_name
 
     def _create_assets_by_img_url(self, news_content_id, file_name):
-        asset_insert_stmt = insert(models.Assets).values(file_name=file_name)
+        asset_insert_stmt = insert(models.Assets).values(file_name=file_name, created_by_id=self.DEFAULT_USER_ID,
+                                                         updated_by_id=self.DEFAULT_USER_ID)
         self.session.execute(asset_insert_stmt)
 
         asset_select_stmt = select(models.Assets).order_by(models.Assets.asset_id.desc())
-        asset = self.session.scalar(asset_select_stmt).first()
+        asset = self.session.scalar(asset_select_stmt)
 
         news_content_asset_stmt = insert(models.NewsContentAssets).values(news_content_id=news_content_id,
                                                                           asset_id=asset.asset_id)
@@ -71,10 +98,15 @@ class DatabaseHandler:
         youtube_urls = parse_service.parse_youtube_urls()
         new_view_data = news_content.view_data if news_content.view_data else {}
         news_content.view_data = new_view_data['youtube'] = youtube_urls
+        news_content.updated_date = datetime.utcnow()
         return news_content
 
     def _extract_main_img_url_into_news_content_main_image(self, news_content, parse_service):
-        absolute_main_image_url = news_content.view_data.get('image_intro', None) if news_content.view_data else ''
+        absolute_main_image_url = news_content.view_data.get('image_intro', None) if news_content.view_data else None
+
+        if not absolute_main_image_url:
+            return news_content
+
         url_server = ParseAbsoluteToDomesticUrlService()
         image_path = url_server.parse_url(absolute_main_image_url)
         file_name = image_path.split('/')[-1]
@@ -82,9 +114,11 @@ class DatabaseHandler:
         asset = self._get_asset_by_file_name(file_name)
         if asset:
             news_content.main_asset_id = asset.asset_id
+            news_content.updated_date = datetime.utcnow()
         else:
             self._upload_file_by_file_path(image_path)
             self._create_assets_by_img_url(news_content.news_content_id, file_name)
+
         return news_content
 
     def _get_asset_by_file_name(self, file_name):
